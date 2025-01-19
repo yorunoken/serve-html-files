a30f07a6379faa7c853ab8095fed0c44c9a61fc0
Yoru <m.f.fetvaci@hotmail.com>
2025-01-19 23:41:23 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+import { serve } from "bun";
+import { readFileSync } from "fs";
+import { join } from "path";
+
+const rootDir = "./public";
+const port = process.env.PORT;
+
+serve({
+    port: port,
+    fetch(req) {
+        const url = new URL(req.url);
+        let filePath = url.pathname === "/" ? "/index.html" : url.pathname;
+
+        try {
+            const fullPath = join(rootDir, filePath);
+            console.log(fullPath);
+            const content = readFileSync(fullPath, "utf-8");
+            return new Response(content, {
+                headers: { "Content-Type": getContentType(filePath) },
+            });
+        } catch (err) {
+            return new Response("File not found", { status: 404 });
+        }
+    },
+});
+
+console.log("Server is running at http://localhost:" + port);
+
+function getContentType(filePath: string) {
+    if (filePath.endsWith(".html")) return "text/html";
+    if (filePath.endsWith(".css")) return "text/css";
+    if (filePath.endsWith(".js")) return "application/javascript";
+    if (filePath.endsWith(".png")) return "image/png";
+    if (filePath.endsWith(".jpg") || filePath.endsWith(".jpeg")) return "image/jpeg";
+    return "text/plain";
+}
